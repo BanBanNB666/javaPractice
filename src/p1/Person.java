b5f6392cd2ec5df4956f9d7da164d84bf99c680a
Ban <2606935387@qq.com>
2023-02-21 18:22:32 +0800

    Initial commit

---
+++ src/p1/Person.java
@@ -0,0 +1,19 @@
+package p1;
+
+/**
+ * @author flz
+ */
+
+public class Person {
+    private String name = null;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    
+
+
+    public static void main(String[] args) {
+
+    }
+}
